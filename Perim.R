7d70091fabcae603d0da8a3fad54b40812a91090
marcvem2AED <marcvem2@alumni.uv.es>
2024-10-15 13:03:59 +0200

    Perímetro rectángulo y cambio PI

--- Perim.R
+++ Perim.R
@@ -1,3 +1,13 @@
 PerimetroCuadrado <- function(L){
   return(L*4)
 }
+
+PerimetroCirculo <- function(r){
+  return(2*3.14159*r)
+}
+
+PerimetroRectángulo <- function(m,n){
+  return(2*m*n)
+}
+
+pi <- 3.14159
